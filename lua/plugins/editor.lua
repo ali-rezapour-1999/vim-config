8c6ff647f24f55c3d5415763e07ab1c715cebe9a
Alirg <alirg>
2024-12-29 12:43:20 -0500

    change for java and python confrom

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,116 +1,141 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		opts = require("configs.conform"),
-	},
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          python = { "black", "isort" }, -- Use black and isort for Python
+          javascript = { "prettier" }, -- Use prettier for JavaScript
+          typescript = { "prettier" }, -- Use prettier for TypeScript
+          html = { "prettier" },    -- Use prettier for HTML
+          css = { "prettier" },     -- Use prettier for CSS
+        },
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_fallback = true, -- Fallback to LSP formatting if no formatter is available
+        },
+      })
+    end,
+  },
 
-	{
-		"numToStr/Comment.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"JoosepAlviste/nvim-ts-context-commentstring",
-		},
-		config = function()
-			require("Comment").setup({
-				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-			})
-		end,
-	},
+  {
+    "ThePrimeagen/refactoring.nvim",
+    keys = {
+      {
+        "<leader>r",
+        function()
+          require("refactoring").select_refactor()
+        end,
+        mode = "v",
+        noremap = true,
+        silent = true,
+        expr = false,
+      },
+    },
+    opts = {},
+  },
 
-	{
-		"mg979/vim-visual-multi",
-		event = "BufRead",
-		config = function() end,
-	},
+  {
+    "numToStr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    config = function()
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
+    end,
+  },
 
-	{
-		"danymat/neogen",
-		keys = {
-			{
-				"<leader>cc",
-				function()
-					require("neogen").generate({})
-				end,
-				desc = "Generate Documentation with Neogen",
-			},
-		},
-		opts = { snippet_engine = "luasnip" },
-		config = function(_, opts)
-			require("neogen").setup(opts)
-		end,
-	},
+  {
+    "mg979/vim-visual-multi",
+    event = "BufRead",
+    config = function() end,
+  },
 
-	{
-		"folke/flash.nvim",
-		opts = {
-			search = {
-				forward = true,
-				multi_window = false,
-				wrap = false,
-				incremental = true,
-			},
-		},
-		config = function(_, opts)
-			require("flash").setup(opts)
-		end,
-	},
+  {
+    "danymat/neogen",
+    keys = {
+      {
+        "<leader>cc",
+        function()
+          require("neogen").generate({})
+        end,
+        desc = "Generate Documentation with Neogen",
+      },
+    },
+    opts = { snippet_engine = "luasnip" },
+    config = function(_, opts)
+      require("neogen").setup(opts)
+    end,
+  },
 
-	{
-		"b0o/incline.nvim",
-		event = "BufReadPre",
-		priority = 1200,
-		config = function(_, opts)
-			require("incline").setup(opts)
-		end,
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-	},
-	{
-		"akinsho/toggleterm.nvim",
-		config = function()
-			require("toggleterm").setup({
-				size = 30,
-				open_mapping = [[,,]],
-				direction = "horizontal",
-			})
-		end,
-	},
-	{
-		"rcarriga/nvim-notify",
-		lazy = false,
-		config = function()
-			local notify = require("notify")
-			notify.setup({
-				position = "top_right",
-				stages = "fade",
-				timeout = 5000,
-				top_down = true,
-				render = "default",
-				border = "rounded",
-				background_colour = "#1e1e2e",
-				max_height = function()
-					return math.floor(vim.o.lines * 0.75)
-				end,
-				max_width = function()
-					return math.floor(vim.o.columns * 0.75)
-				end,
-				on_open = function(win)
-					vim.api.nvim_win_set_config(win, {
-						border = "rounded",
-						relative = "editor",
-						anchor = "NE",
-						row = 1,
-						col = vim.o.columns - 1,
-					})
-				end,
-			})
-			vim.notify = notify
-		end,
-	},
+  {
+    "folke/flash.nvim",
+    opts = {
+      search = {
+        forward = true,
+        multi_window = false,
+        wrap = false,
+        incremental = true,
+      },
+    },
+    config = function(_, opts)
+      require("flash").setup(opts)
+    end,
+  },
+
+  {
+    "b0o/incline.nvim",
+    event = "BufReadPre",
+    priority = 1200,
+    config = function(_, opts)
+      require("incline").setup(opts)
+    end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup({
+        size = 30,
+        open_mapping = [[,,]],
+        direction = "horizontal",
+      })
+    end,
+  },
+  {
+    enabled = false,
+    "folke/flash.nvim",
+    opts = {
+      search = {
+        forward = true,
+        multi_window = false,
+        wrap = false,
+        incremental = true,
+      },
+    },
+  },
+  {
+    "echasnovski/mini.bracketed",
+    event = "BufReadPost",
+    config = function()
+      local bracketed = require("mini.bracketed")
+      bracketed.setup({
+        file = { suffix = "" },
+        window = { suffix = "" },
+        quickfix = { suffix = "" },
+        yank = { suffix = "" },
+        treesitter = { suffix = "n" },
+      })
+    end,
+  },
 }
